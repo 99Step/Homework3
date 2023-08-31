94ce683dbfbb5f35f5215ca356a666b68bf328c4
Stepan <stepan.ostapchuk@betconstruct.com>
2023-08-31 12:02:32 +0300

    Homework3 update task1

--- l3_task1.erl
+++ l3_task1.erl
@@ -6,9 +6,9 @@ first_word(BinText) ->
   first_word(BinText, <<>>).
 
 first_word(<<32, _Rest/binary>>, Acc) ->
-  io:format("~tp~n", [Acc]);
+  Acc;
 first_word(<<X/utf8, Rest/binary>>, Acc)->
-  first_word(Rest, <<Acc/binary, X/utf8>>);
+  first_word(Rest, <<Acc/binary, X>>);
 first_word(<<>>, Acc) ->
   Acc.
 
